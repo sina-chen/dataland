46d2e704db7d282774dd1ccca0330f103914297a
Sina Chen <sina.chen@uni-konstanz.de>
2023-08-29 17:21:13 +0200

    update code

--- code/dataland_helper_functions.R
+++ code/dataland_helper_functions.R
@@ -280,6 +280,87 @@ trends_update <- function(file = "trends.csv", path = "data/"){
 }
 
 
+#' Write/update polls and trends and generate plot
+#'
+#' @param make_plot Logical, indicating whether generate plot. Default is TRUE.
+#'
+#' @examples
+dataland2024_polls_trends <- function(make_plot =T){
 
+  tryCatch({  
+    
+    # get/update polls
+    polls_update()
+    
+  }, 
+  
+  error = function(e) {
+    cat(Sys.time(), paste0("An error occured while updating/writing polls:", e, 
+                           "\n"), 
+        file = log_file, append = TRUE)
+    message(paste0("Error: Something went wrong while updating/writing polls, check ", 
+                   log_file, "."))
+  })
 
+  tryCatch({    
+    
+    # get/update trends
+    trends_update()
+  
+  }, 
+  
+  error = function(e) {
+    cat(Sys.time(), paste0("An error occured while updating/writing trends:", e, 
+                           "\n"), 
+        file = log_file, append = TRUE)
+    message(paste0("Error: Something went wrong while updating/writing trends, check ", 
+                   log_file, "."))
+  })  
+
+  tryCatch({  
+    
+    if(make_plot == T){
+      
+      # read data
+      polls <- read.csv("data/polls.csv")
+      trends <- read.csv("data/trends.csv")
+      
+      # reshape to long for visual inspection
+      trends_long <- trends %>%
+        pivot_longer(cols = -date, names_to = "candidate", values_to = "trends") %>%
+        mutate(date = as.Date(date),
+               candidate = str_to_title(candidate))
+      polls_long <- polls %>%
+        pivot_longer(cols = -c(date, pollster, n, overseas, alternate),
+                     names_to = "candidate", values_to = "support") %>%
+        mutate(date = as.Date(date),
+               candidate = str_to_title(candidate))
+      
+      polls_trends_plot <- ggplot() +
+        geom_point(data = polls_long, aes(x = rev(date), y = support, 
+                                          color = candidate)) +
+        geom_line(data = trends_long, aes(x = rev(date), y = trends, 
+                                          color = candidate)) +
+        theme_bw() +
+        labs(x = "Date", y = "Support", color = "Candidate", 
+             caption = "The points display observed poll results, while the lines illustrate the trends of the 7-day average.") +
+        theme(text = element_text(size = 16))
+      
+      ggsave(filename = "plots/polls_trends_plot.png", 
+             plot = polls_trends_plot, 
+             width = 12, height = 6, 
+             bg='#ffffff')
+      print("Polls & trends plot is generated and saved in the 'plots' folder.")
+    }
+  }, 
+  
+  error = function(e) {
+    cat(Sys.time(), paste0("An error occured while generating polls/trends plot:", e, 
+                           "\n"), 
+        file = log_file, append = TRUE)
+    message(paste0("Error: Something went wrong while generating polls/trends plot, check ", 
+                   log_file, "."))
+  })  
+ 
+}
 
